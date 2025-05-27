fb30d5a3eecc009401626ad552c6ded2a4cf230f
herik222 <121726566+herik222@users.noreply.github.com>
2025-05-27 16:31:52 +0000

    fim

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 
-
-
 int main() {
-    printf("Desafio Super Trunfo!\n");
+    printf("Desafio Super Trunfo! \n");
+
+    char estado[10];
+    char cidade[20];
+    char codigo[5];
+int populacao;
+float area;
+float pib;
+int turismo;
+
+printf ("Nome do estado: \n");
+scanf("%s", &estado);
+
+printf("codigo: \n");
+scanf("%s", &codigo);
+
+printf("nome da cidade:: \n");
+scanf("%s", &cidade);
+
+printf("total de habitantes \n");
+scanf("%d", &populacao);
+
+printf("area: \n");
+scanf("%f", &area);
+
+printf("pib: \n");
+scanf("%f", &pib);
+
+printf("numero de pontos turisticos: \n");
+scanf("%d", &turismo);
 
     return 0;
 }
